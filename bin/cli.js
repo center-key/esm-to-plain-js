6513ba384e6eaafe88d52228cdb5b7b5859590bf
dpilafian <dem@centerkey.com>
2022-11-19 02:19:27 -0800

    Use cli-argv-util to parse flags

--- bin/cli.js
+++ bin/cli.js
@@ -23,42 +23,36 @@
 import { esmToPlainJs } from '../dist/esm-to-plain-js.js';
 import chalk from 'chalk';
 import log   from 'fancy-log';
+import { cliArgvUtil } from 'cli-argv-util';
 
-// Parameters
-const validFlags =  ['cd', 'note', 'quiet'];
-const args =        process.argv.slice(2);
-const flags =       args.filter(arg => /^--/.test(arg));
-const flagMap =     Object.fromEntries(flags.map(flag => flag.replace(/^--/, '').split('=')));
-const flagOn =      Object.fromEntries(validFlags.map(flag => [flag, flag in flagMap]));
-const invalidFlag = Object.keys(flagMap).find(key => !validFlags.includes(key));
-const params =      args.filter(arg => !/^--/.test(arg));
-
-// Data
-const source = params[0];
-const target = params[1];
+// Parameters and flags
+const validFlags = ['cd', 'note', 'quiet'];
+const cli =        cliArgvUtil.parse(validFlags);
+const source =     cli.params[0];
+const target =     cli.params[1];
 
 // Reporting
 const printReport = (result) => {
    const name =   chalk.gray('esm-to-plain-js');
    const origin = chalk.blue.bold(result.origin);
    const dest =   chalk.magenta(result.dest);
-   const arrow =  chalk.gray.bold(' ⟹  ');  //extra space for alignment
+   const arrow =  chalk.gray.bold('→');
    const info =   chalk.white(`(${result.duration}ms)`);
    log(name, origin, arrow, dest, info);
    };
 
 // Copy File
 const error =
-   invalidFlag ?       'Invalid flag: ' + invalidFlag :
-   params.length > 2 ? 'Extraneous parameter: ' + params[2] :
-   !source ?           'Missing source file.' :
-   !target ?           'Missing target file.' :
+   cli.invalidFlag ?     cli.invalidFlagMsg :
+   cli.paramsCount > 2 ? 'Extraneous parameter: ' + cli.params[2] :
+   !source ?             'Missing source file.' :
+   !target ?             'Missing target file.' :
    null;
 if (error)
    throw Error('[esm-to-plain-js] ' + error);
 const options = {
-   cd: flagMap.cd ?? null,
+   cd: cli.flagMap.cd ?? null,
    };
 const result = esmToPlainJs.transform(source, target, options);
-if (!flagOn.quiet)
+if (!cli.flagOn.quiet)
    printReport(result);
